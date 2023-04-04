6a90ce2e7c54b2974f80471b0bc48d4bf71bd93d
Kevin <kevin_zino@yahoo.com>
2023-04-05 00:32:38 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import * as Phaser from 'phaser';
+import './style.css'
+
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+	preload: preload,
+	create: create,
+	update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
