45f5b6bd12e91edd6cb6763e01ded4523a9ca02a
Kevin <kevin_zino@yahoo.com>
2023-04-07 01:01:56 +0300

    Render platforms on the game scene

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,29 @@ import './style.css'
 
 class PhaserMan extends Phaser.Scene {
   constructor() {
-	super({ key: 'PhaserMan' });
+	super({key: 'PhaserMan'});
   }
 
   preload() {
 	this.load.image('sky', 'src/assets/sky.png');
-    this.load.image('ground', 'src/assets/platform.png');
-    this.load.image('star', 'src/assets/star.png');
-    this.load.image('bomb', 'src/assets/bomb.png');
-    this.load.spritesheet('dude',
-      'src/assets/dude.png',
-      { frameWidth: 32, frameHeight: 48 }
-    );
+	this.load.image('ground', 'src/assets/platform.png');
+	this.load.image('star', 'src/assets/star.png');
+	this.load.image('bomb', 'src/assets/bomb.png');
+	this.load.spritesheet('dude',
+	  'src/assets/dude.png',
+	  {frameWidth: 32, frameHeight: 48}
+	);
   }
 
   create() {
-    this.add.image(400, 300, 'sky');
+	const platforms = this.physics.add.staticGroup();
+
+	this.add.image(400, 300, 'sky');
+
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	platforms.create(600, 400, 'ground');
+	platforms.create(50, 250, 'ground');
+	platforms.create(750, 220, 'ground');
   }
 
   update() {
@@ -30,7 +37,15 @@ const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: PhaserMan
+  scene: PhaserMan,
+  physics: {
+	default: 'arcade',
+	arcade: {
+	  gravity: { y: 300 },
+	  debug: false
+	}
+  }
+
 };
 
 const game = new Phaser.Game(config);
