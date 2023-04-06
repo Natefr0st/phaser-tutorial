90199b857bcfdea870ac5a4950e38cc42f9ecac7
Kevin <kevin_zino@yahoo.com>
2023-04-07 00:19:32 +0300

    Refactoring and Preload game assets

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,35 @@ import * as Phaser from 'phaser';
 import './style.css'
 
 
+class PhaserMan extends Phaser.Scene {
+  constructor() {
+	super({ key: 'PhaserMan' });
+  }
+
+  preload() {
+	this.load.image('sky', 'src/assets/sky.png');
+    this.load.image('ground', 'src/assets/platform.png');
+    this.load.image('star', 'src/assets/star.png');
+    this.load.image('bomb', 'src/assets/bomb.png');
+    this.load.spritesheet('dude',
+      'src/assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+    );
+  }
+
+  create() {
+    this.add.image(400, 300, 'sky');
+  }
+
+  update() {
+  }
+}
+
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: {
-	preload: preload,
-	create: create,
-	update: update
-  }
+  scene: PhaserMan
 };
 
 const game = new Phaser.Game(config);
-
-function preload() {
-}
-
-function create() {
-}
-
-function update() {
-}
